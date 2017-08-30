e13350539a667a078db3b0e59cdb3e630508b74b
Sebastian Velasco <sebasvelasco353@hotmail.com>
2017-08-30 00:43:23 -0500

    starting to work on add imgtoDb

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
+
+//const dbConnection = require('./firebaseConnection.js'); //js file that connects to database
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const app = express();
+
+//------------
+app.get('/addImgToDb', function(req, res) {
+  res.json('dd');
+});
+
+exports.app = functions.https.onRequest(app);
