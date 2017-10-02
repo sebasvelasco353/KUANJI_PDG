dde076fc49ce57279eed1a2a5ba682400c4ede73
Sebastian Velasco <sebasvelasco353@hotmail.com>
2017-10-02 01:08:30 -0500

    Added the routes of the functions i must finish: retreiveLinks, all tags, specific tag and also the add to db functions

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,58 @@
-
-//const dbConnection = require('./firebaseConnection.js'); //js file that connects to database
-const functions = require('firebase-functions');
-
+const Clarifai = require('clarifai');
+const dbConnection = require('./firebaseConnection.js'); //js file that connects to database
+const functions = require('firebase-functions'); //Functions
+const admin = require("firebase-admin");
 const express = require('express');
 const app = express();
-
-//------------
-app.get('/addImgToDb', function(req, res) {
-  res.json('dd');
+var db = admin.database();
+var ref = db.ref('/');
+const clarifaiApp = new Clarifai.App({
+  apiKey: 'b71dea8696994f2f896b4cfa9f667b7d'
 });
+
+
+//Gets
+//------------ Function used for predicting the image sent by the front end to firebase
+app.get('/predict', function(req, res) {
+  var toPredict = req.query.link; //https://samples.clarifai.com/metro-north.jpg
+  clarifaiApp.models.predict(Clarifai.GENERAL_MODEL, toPredict).then(
+    function(response) {
+      res.json(response.rawData);
+    },
+    function(err) {
+      // there was an error
+      res.send("Error");
+    }
+  );
+});
+
+//------------ Function used for retreiving all image links in the format link -> tag 1, 2 and 3
+app.get('/retreiveAllLinks', function(req, res) {
+
+});
+
+//------------ Function used for retreiving all image links from All tags in the format tag -> link 1, 2 ... n
+app.get('/retreiveAllTags', function(req, res) {
+
+});
+
+//------------ Function used for retreiving all image links from one specific tag in the format tag -> link 1, 2 ... n
+app.get('/retreiveSpecificTag', function(req, res) {
+
+});
+
+//Posts
+//------------ Function used for adding the tags of the image to the tags on the database
+app.post('/addTagsToDb', function(req, res) {
+
+});
+
+
+//------------ Function used for adding the link of the image to the links on the database
+app.post('/addLinksToDb', function(req, res) {
+
+});
+
+
 
 exports.app = functions.https.onRequest(app);
